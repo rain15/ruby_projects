5746ce4200d73b139a151bf5aef3374d353c6bea
Hema <hema.pillay@gmail.com>
2016-03-09 22:30:19 +0000

    Added

---
+++ analyzer.rb
@@ -0,0 +1,12 @@
+puts 'Hello, enter your first name'
+
+first_name = gets.chomp
+
+puts 'What is your last name?'
+last_name = gets.chomp
+
+puts "Welcome #{first_name} #{last_name} to the playground"
+puts "length of your first name is #{first_name.length}."
+puts "length of your last name is #{last_name.length}."
+puts "reverse of your first name is #{first_name.reverse}."
+puts "reverse of your last name is #{last_name.reverse}."
---
+++ playground.rb
@@ -0,0 +1,5 @@
+puts "Hello, enter your first name: "
+
+first_name = gets.chomp
+
+puts "Welcome #{first_name} to the playground!"
