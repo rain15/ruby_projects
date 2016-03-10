13b6ae176ace7677b362b4967697b9c9955d7716
Hema <hema.pillay@gmail.com>
2016-03-10 00:01:14 +0000

    added

---
+++ numbers.rb
@@ -0,0 +1,18 @@
+puts "Please enter your first number: "
+first_num = gets.chomp.to_i
+
+puts "Please enter your second number:"
+second_num = gets.chomp.to_i
+
+result = first_num * second_num
+
+# the line below
+#puts "The result of multiplying both numbers is " + result.to_s
+
+#you can also do this
+puts "The result of multiplying both numbers is #{result.to_s}"
+
+=begin
+testing multi line
+comments
+=end
